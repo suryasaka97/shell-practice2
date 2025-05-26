38ab53c9d61cbe6709022d256bb5d70e5fba686c
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-26 18:06:16 +0530

    read script updated with -s and -p flags v2

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+
+# -p : prompt
+# -s : silent
+
 echo "Enter two characters using space"
 
 read person1 person2
 
+echo""
 
 
 echo "$person1:: Hey $person2, How are you?"
@@ -12,18 +17,30 @@ echo "$person1: I am fine too. What's up?"
 echo "$person2:: Nohting, just going to Mars now, will you come?"
 echo "$person1:: Sorry, you carry on! I will come once you come back"
 
+echo " "
+echo" "
 
-read -p "enter the right guy whom we need to surrender" god
+
+read -p "enter the right guy whom we need to surrender: " god
 
 echo "The creator $god"
 
-echo "enter your crush name, no wories it hidden"
+echo ""
+echo ""
+
+echo "enter your crush name, no wories it is hidden"
 
 read -s crush
 
+echo " "
+
 echo "your crush name is $crush"
 
+echo ""
 
-read -p -s "your input is hidden" test
+echo ""
+
+
+read -sp "your input is hidden: " test
 
 echo $test
