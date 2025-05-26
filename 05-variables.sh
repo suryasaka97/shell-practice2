ba61a6d26cc14cdf82960221fb4be0a722fee25d
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-26 17:50:24 +0530

    read script

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter two characters using space"
+
+read person1 person2
+
+
+
+echo "$person1:: Hey $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you doing"
+echo "$person1: I am fine too. What's up?"
+echo "$person2:: Nohting, just going to Mars now, will you come?"
+echo "$person1:: Sorry, you carry on! I will come once you come back"
