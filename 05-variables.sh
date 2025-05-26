c0d372af5513408ef5cd031bd6648abf2f220d5e
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-26 18:00:43 +0530

    read script updated with -s and -p flags

--- 05-variables.sh
+++ 05-variables.sh
@@ -11,3 +11,19 @@ echo "$person2:: Hello $person1, I am fine. How are you doing"
 echo "$person1: I am fine too. What's up?"
 echo "$person2:: Nohting, just going to Mars now, will you come?"
 echo "$person1:: Sorry, you carry on! I will come once you come back"
+
+
+read -p "enter the right guy whom we need to surrender" god
+
+echo "The creator $god"
+
+echo "enter your crush name, no wories it hidden"
+
+read -s crush
+
+echo "your crush name is $crush"
+
+
+read -p -s "your input is hidden" test
+
+echo $test
