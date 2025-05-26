2cdb13c6eee103771a3f740b16c3e1eb32219a8e
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-26 17:37:44 +0530

    Arguments script

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# In this scipt we are learning how to use Arguments
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hey $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you doing"
+echo "$person1: I am fine too. What's up?"
+echo "$person2:: Nohting, just going to Mars now, will you come?"
+echo "$person1:: Sorry, you carry on! I will come once you come back"
