24ddbad903a15554c72b0b2a0ed482961c5bf94c
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-26 16:53:10 +0530

    variables script

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+person1="Jeff"
+person2="sundar"
+
+echo "$person1:: Hey $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you doing"
+echo "$person1: I am fine too. What's up?"
+echo "$person2:: Nohting, just going to Mars now, will you come?"
+echo "$person1:: Sorry, you carry on! I will come once you come back"
