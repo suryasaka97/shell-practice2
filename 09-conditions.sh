529438b716902becf2a972aad73895da4375537f
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-27 15:08:45 +0530

    conditional statements

---
+++ 09-conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#-gt  greter than
+#-lt  less than
+#-eq  equal
+#-ne  not equal
+#-ge  greater or equal
+#-le  lesser or equal
+
+a=$1
+
+if [ $a -gt 60 ]
+then
+    echo "He is selected for tests"
+
+else
+    echo "selected for T2o's"
+
+fi      
