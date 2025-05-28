eb9b011e8ea17fc943c48b040fa18eb772f70666
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-28 17:09:53 +0530

    checking root user

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -2,9 +2,20 @@
 
 user=$(id -u)
 
-if [ $user -eq 0 ]
-then
-echo "Installing............starts"
+# if [ $user -eq 0 ]
+# then
+# echo "Installing............starts"    ###here for readability you can give space" but no need as per syntax!
+# else
+# echo "Please run this script using Root Access"
+# fi 
+
+############Always use fail early and fail fast############################
+
+
+if [ $user -ne 0]
+then   
+    echo "please run script using sudo or root"
+    exit 0
 else
-echo "Please run this script using Root Access"
-fi 
+    echo "Installation starts"
+fi        
