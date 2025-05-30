f4e7a6a3de547c51da5dcfbda0237fea490a9949
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-30 15:35:15 +0530

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\e[31m Royal Challemgers Bengaluru"
+
+echo "Virat Kohli"
+
+# Above both will be in red color"
+
+#for reset use \e[0m #
+
+echo " \e[0m Virat Kohli"
---
+++ python_loop.py
@@ -0,0 +1,4 @@
+frenchises=[rcb, mi, gt,pbks]
+i=0
+for i in range(rcb,pbks):
+    print(f"best batsman{i} bestblower{i}best captain{i}")
