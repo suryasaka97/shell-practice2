1a816a75da926288aff802887a13097faa5cd59b
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-30 15:57:26 +0530

    colors script

---
+++ 13-colors.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+
+############# General syntax for function  ####################
+
+# Assign colors to the script #
+Red=\e[31m
+Green=\e[32m
+Yellow=\e[33m
+Reset=\e[0m
+
+
+
+user=$(id -u)
+
+if [ $user -ne 0 ]
+then
+    echo -e "$Red please run this script with root privileges$Reset"
+    exit 1
+else
+    echo -e "$Green Great!...you ae running as sudo this $0 script as root user$Reset"
+fi
+
+# Function for Validating sytem software installation #
+
+validate() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$Red Installation of $2 failed please check$Reset"
+        exit 1
+    else
+        echo -e "$Green installed $2 successfully$Reset"
+    fi  
+}
+
+
+
+# Installing mysql script #
+
+echo -e "$Yellow Checking....whether you have mysql installed or not!$Reset"
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo -e "$Red mysql is not installed...installing...$Reset"
+    dnf install mysql -y
+    validate $? mysql
+else
+    echo -e "$Yellow mysql is already installed$Reset"
+fi    
+
+# Installing python3 #
+
+echo -e  "$Yellow Checking....whether you have python installed or not!$Reset"
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then
+    echo -e "$Red python3 is not installed..installing$Reset"
+    dnf install python3 -y
+    validate $? python3
+else
+    echo -e "$Yellow python3 is already installed$Reset"
+fi
+
+
+# Installing Nginx #
+
+echo -e "$Yellow Checking....whether you have nginx installed or not!$Reset"
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo -e "$Red nginx is not installed..installing$Reset"
+    dnf install nginx -y
+    validate $? nginx
+else
+    echo -e "$Yellow nginx is already installed$Reset"
+fi
+
+
+
+
+
+
