03e6ca2c1747a1f29685eb4424006c035c7a3f2b
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-05-31 11:51:30 +0530

    log_script tee update

--- update_remove.sh
+++ update_remove.sh
@@ -3,7 +3,11 @@
 
 echo -e "\e[33mupdating all \e[32mpackages\e[0m"
 
+echo -e  "\e[36mRunning.....yum.....command\e[0m"
+
 sudo yum update -y
+
+echo -e  "\e[36mRunning.....dnf.....command\e[0m"
 
 sudo dnf upgrade -y
 
