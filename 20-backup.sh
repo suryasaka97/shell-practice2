37aaec24d3bebda5dc8050665c8eb3366e272f97
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-07-03 17:40:40 +0530

    deleting old logs

--- 20-backup.sh
+++ 20-backup.sh
@@ -69,7 +69,7 @@ then
     ##cretae Zip file now in dest_dir ###app-logs-date.zip (here date contains current hour,minutes,seconds)
     TIMESTAMP=$(date +%F-%H-%M-%s)
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    echo $files_to_delete | zip -@ "$ZIP_FILE"
+    echo $files_to_delete #| zip -@ "$ZIP_FILE"
 else
     echo -e "$R No files $N to zip"
 fi        
