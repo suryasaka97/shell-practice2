7a0a7b9d0ca1470fda79127d6881abbe514b2bf7
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-07-08 17:35:44 +0530

    disk space

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+threshold_value=1
 
 while IFS= read eachline
 do
     usage=$(echo $eachline | awk '{print $6}' | cut -d "%" -f1)
     partition=$(echo $eachline | awk '{print $7}')
-    echo $partition:$usage 
+    if [ $usage -gt $partition ]
+    then
+        MSG="High disk usage on $partition:$usage"
+    fi    
 done <<< $DISK_USAGE
