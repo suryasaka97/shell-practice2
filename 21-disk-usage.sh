c657a291241ca1e13d9b20f34d6835186ca4eb6c
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-07-08 17:11:34 +0530

    disk space

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 
 while IFS= read eachline
 do
-    usage=$(echo $eachline | awk '{print $6}' | cut -d "." -f1)
+    usage=$(echo $eachline | awk '{print $6}' | cut -d "%" -f1)
     partition=$(echo $eachline | awk '{print $7}')
     echo $partition:$usage 
 done <<< $DISK_USAGE
